f7270e1424b1596902a493c71d925c2925f60e1c
Levi Lesches <levilesches@gmail.com>
2019-09-18 22:58:45 -0400

    Added CloudStorage service

--- lib/services.dart
+++ lib/services.dart
@@ -1,5 +1,6 @@
 library admin_services;
 
-export "src/services/firestore.dart";
 export "src/services/auth.dart";
+export "src/services/cloud_storage.dart";
+export "src/services/firestore.dart";
 export "src/services/reader.dart";
---
+++ lib/src/services/cloud_storage.dart
@@ -0,0 +1,46 @@
+import "dart:io";
+import "package:flutter/foundation.dart";
+import "package:firebase_storage/firebase_storage.dart";
+
+class CloudStorage {
+	static final StorageReference root = FirebaseStorage().ref();
+
+	final Directory dir;
+	final String publication;
+
+	CloudStorage({
+		@required String path, 
+		@required this.publication
+	}) : 
+		dir = Directory("$path/publications");
+
+	String getPath(String path) => "${dir.path}/$path";
+
+	Future<Map<String, String>> get metadata async => 
+		(await root.child(publication).getMetadata()).customMetadata;
+
+	Future<void> getImage() async {
+		final File file = File(getPath("$publication.png"));
+		if (!file.existsSync()) {
+			await root.child("$publication/$publication.png").writeToFile(file).future;
+		}
+	}
+
+	Future<void> getIssue(String issue) => 
+		root.child(issue).writeToFile(File(getPath(issue))).future;
+
+	Future<void> uploadImage(String path) => 
+		root.child("$publication/$publication.png").putFile(File(path)).onComplete;
+
+	Future<void> uploadIssue(String issue, String path) => 
+		root.child(issue).putFile(File(path)).onComplete;
+
+	Future<void> uploadMetadata(Map<String, String> metadata) =>
+		root.child("$publication/issues.txt").updateMetadata(
+			StorageMetadata(customMetadata: metadata)
+		);
+
+	Future<void> deleteIssue(String issue) => root.child(issue).delete();
+
+	void deleteLocal() => dir.deleteSync(recursive: true);
+}
