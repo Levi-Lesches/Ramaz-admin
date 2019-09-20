3eaa95e70fae69da2a2277c22931a0629573776d
Levi Lesches <levilesches@gmail.com>
2019-09-19 21:58:42 -0400

    Tidied up Services library according to linter

--- lib/src/services/firestore.dart
+++ lib/src/services/firestore.dart
@@ -1,26 +1,32 @@
-import "package:cloud_firestore/cloud_firestore.dart" as FB;
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import "package:cloud_firestore/cloud_firestore.dart" as cloud_firestore;
 
 import "auth.dart";
 
 class Firestore {
-	static final FB.Firestore firestore = FB.Firestore.instance;
+	static final cloud_firestore.Firestore firestore = 
+		cloud_firestore.Firestore.instance;
 
-	static const String ADMINS = "admin";
-	static const String CALENDAR = "calendar";
+	static const String adminsCollectionName = "admin";
+	static const String calendarCollectionName = "calendar";
 
-	static final FB.CollectionReference admins = firestore.collection(ADMINS);
-	static final FB.CollectionReference calendar = firestore.collection(CALENDAR);
+	static final cloud_firestore.CollectionReference admins = 
+		firestore.collection(adminsCollectionName);
+
+	static final cloud_firestore.CollectionReference calendar = 
+		firestore.collection(calendarCollectionName);
 
 
 	static Future<Map<String, dynamic>> get admin async => 
 		(await admins.document(await Auth.email).get()).data;
 
 	static Future<void> saveAdmin(Map<String, dynamic> json) async => 
-		(await admins.document(await Auth.email).setData(json));
+		admins.document(await Auth.email).setData(json);
 
-	static Future<void> saveCalendar(int month, Map<String, dynamic> json) async =>
-		(await calendar.document(month.toString()).setData(json));
+	static Future<void> saveCalendar(int month, Map<String, dynamic> json) =>
+		calendar.document(month.toString()).setData(json);
 
-	static Stream<FB.DocumentSnapshot> getCalendar(int month) => 
+	static Stream<cloud_firestore.DocumentSnapshot> getCalendar(int month) => 
 		calendar.document(month.toString()).snapshots();
 }
