5e2f9d0136da2ac8edd4416c31919888cb4a7965
Levi Lesches <levilesches@gmail.com>
2019-09-19 22:55:04 -0400

    Added PublicationMonthsList

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -4,5 +4,6 @@ export "src/widgets/calendar_tile.dart";
 export "src/widgets/day_builder.dart";
 export "src/widgets/model_listener.dart";
 export "src/widgets/period_tile.dart";
+export "src/widgets/publication_tile.dart";
 export "src/widgets/services.dart";
 export "src/widgets/special_builder.dart";
